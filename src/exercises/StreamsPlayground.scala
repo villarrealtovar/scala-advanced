92bccaac349161c05e3565846f309a02c5295221
Jose Villarreal <>
2021-05-07 09:52:24 -0500

    3.9 Exercises Lazy Evaluation

--- src/exercises/StreamsPlayground.scala
+++ src/exercises/StreamsPlayground.scala
@@ -1,5 +1,7 @@
 package exercises
 
+import scala.annotation.tailrec
+
 
 abstract  class MyStream[+A] {
   def isEmpty: Boolean
@@ -7,21 +9,139 @@ abstract  class MyStream[+A] {
   def tail: MyStream[A]
 
   def #::[B >: A](element: B): MyStream[B] // prepend operator
-  def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // concatenate two streams
+  def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] // concatenate two streams
 
   def foreach(f: A => Unit): Unit
   def map[B](f: A => B): MyStream[B]
-  def flatMap[B](f: A => B): MyStream[B]
+  def flatMap[B](f: A => MyStream[B]): MyStream[B]
   def filter(predicate: A=> Boolean): MyStream[A]
 
   def take(n: Int): MyStream[A] // takes the first n elements out of this stream
-  def takeAsList(n: Int): List[A]
+  def takeAsList(n: Int): List[A] = take(n).toList()
+
+  /*
+    [1 2 3].toList([]) =
+    [2 3].toList([1]) =
+    [3].toList([2 1]) =
+    [].toList([3 2 1]) =
+    [1 2 3]
+   */
+  @tailrec
+  final def toList[B >: A](acc: List[B] = Nil): List[B] =
+    if (isEmpty) acc.reverse
+    else tail.toList(head :: acc)
+}
+
+object EmptyStream extends MyStream[Nothing] {
+  def isEmpty: Boolean = true
+  def head: Nothing  = throw new NoSuchElementException
+  def tail: MyStream[Nothing]  = throw new NoSuchElementException
+
+  def #::[B >: Nothing](element: B): MyStream[B]  = new Cons(element, this)
+  def ++[B >: Nothing](anotherStream: => MyStream[B]): MyStream[B]  = anotherStream
+
+  def foreach(f: Nothing => Unit): Unit  = ()
+  def map[B](f: Nothing => B): MyStream[B]  = this
+  def flatMap[B](f: Nothing => MyStream[B]): MyStream[B]  = this
+  def filter(predicate: Nothing => Boolean): MyStream[Nothing] = this
+
+  def take(n: Int): MyStream[Nothing]  = this
+}
+
+class Cons[+A](hd: A, tl: => MyStream[A]) extends MyStream[A] {
+  def isEmpty: Boolean = false
+
+  override val head: A = hd
+  override lazy val tail: MyStream[A] = tl // call by need
+
+  /*
+    val someStream = new Cons(1, EmptyStream)
+    val prepend = 1 #:: s
+    = new Cons(1, s)
+   */
+  def #::[B >: A](element: B): MyStream[B] = new Cons(element, this)
+  def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
+
+  def foreach(f: A => Unit): Unit = {
+    f(head)
+    tail.foreach(f)
+  }
+
+  /*
+    val s = new Cons(1, ?)
+    mapped = s.map(_ + 1) = new Cons(2, s.tail.map(_+1))
+    ... mapped.tail
+   */
+  def map[B](f: A => B): MyStream[B] = new Cons(f(head), tail.map(f)) // preserve lazy eval!
+  def flatMap[B](f: A => MyStream[B]): MyStream[B] = f(head) ++ tail.flatMap(f)
+
+  def filter(predicate: A=> Boolean): MyStream[A] =
+    if (predicate(head)) new Cons(head, tail.filter(predicate))
+    else tail.filter(predicate) // preserve lazy eval!
+
+  def take(n: Int): MyStream[A] = {
+    if (n <= 0) EmptyStream
+    else if (n == 1) new Cons(head, EmptyStream)
+    else new Cons(head, tail.take(n-1)) // preserve lazy eval!
+  }
 }
 
 object MyStream {
-  def from[A](start: A)(generator: A => A): MyStream[A] = ???
+  def from[A](start: A)(generator: A => A): MyStream[A] =
+    new Cons(start, MyStream.from(generator(start))(generator))
 }
 
-object StreamsPlayground extends App { // min 1.25
+object StreamsPlayground extends App {
+  val naturals = MyStream.from(1)(_ + 1)
+  println(naturals.head)
+  println(naturals.tail.head)
+  println(naturals.tail.tail.head)
 
+  val startFrom0 = 0  #:: naturals // naturals.#::(0)
+  println(startFrom0)
+  startFrom0.take(10000).foreach(println)
+
+  // map, flatMap
+  println(startFrom0.map(_ * 2).take(100).toList())
+  println(startFrom0.flatMap(x => new Cons(x, new Cons(x + 1, EmptyStream))).take(10).toList())
+  println(startFrom0.filter(_ < 10).take(10).take(20).toList())
+
+  // Exercises on streams
+  // 1 - stream of Fibonacci numbers
+  // 2 - stream of prime numbers with Eratosthenes' sieve:
+  /*
+    Eratosthenes' sieve works:
+
+    [2 3 4 ...]
+    filter out all numbers divisible by 2
+    [2 3 5 7 9 11 ...]
+    filter out all number divisible by 3
+    [2 3 5 7 11 13 17...]
+    filter out all number divisible by 5
+    .....
+   */
+
+
+  /*
+    [first, [....
+    [first, fibonacci(second, first + second)
+   */
+  def fibonacci(first: BigInt, second: BigInt): MyStream[BigInt] =
+    new Cons(first, fibonacci(second, first + second))
+
+  println(fibonacci(1, 1).take(100).toList())
+
+  /*
+    [ 2 3 4 5 6 7 8 9 10 11 12 ...
+    [ 2 3 5 7 9 11 13 ...
+    [ 2 eratosthenes applied to (number filtered by n % 2 ! == 0)
+    [ 2 3 eratosthenes applied to [ 5 7 9 11 ...] filter by n % 3 != 0
+    [ 2 3 5 .... .... ...
+   */
+  // eratosthenes stive
+  def eratosthenes(numbers: MyStream[Int]): MyStream[Int] =
+    if (numbers.isEmpty) numbers
+    else new Cons(numbers.head, eratosthenes(numbers.tail.filter(_ % numbers.head != 0)))
+
+  println(eratosthenes(MyStream.from(2)(_ + 1)).take(100).toList())
 }
